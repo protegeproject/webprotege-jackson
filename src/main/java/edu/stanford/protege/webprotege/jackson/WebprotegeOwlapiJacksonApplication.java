89cdf5a36aaff0976bbf4dbc6920466a42811fb9
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-13 13:22:29 -0700

    Added conditional bean definition for owl datafactory

--- src/main/java/edu/stanford/protege/webprotege/jackson/WebprotegeOwlapiJacksonApplication.java
+++ src/main/java/edu/stanford/protege/webprotege/jackson/WebprotegeOwlapiJacksonApplication.java
@@ -3,7 +3,10 @@ package edu.stanford.protege.webprotege.jackson;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.Conditional;
 import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 import javax.naming.spi.ObjectFactory;
@@ -16,6 +19,7 @@ public class WebprotegeOwlapiJacksonApplication {
 	}
 
 	@Bean
+	@ConditionalOnMissingBean
 	OWLDataFactory dataFactory() {
 		return new OWLDataFactoryImpl();
 	}
